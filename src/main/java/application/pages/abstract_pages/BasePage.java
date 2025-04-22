dbd3a808c336605fee53f2d7763b94716b8a7136
Yoann DAGAND <yoann.dagand95@gmail.com>
2025-04-22 11:15:07 +0200

    Comment BasePage

--- src/main/java/application/pages/abstract_pages/BasePage.java
+++ src/main/java/application/pages/abstract_pages/BasePage.java
@@ -4,10 +4,21 @@ import application.utils.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+/**
+ * Classe de base, héritée par toutes les autres pages.
+ * Permet d'initialiser le driver et les @FindBy via l'initialisation des éléments avec PageFactory
+ */
 public abstract class BasePage {
 
+    /**
+     * Le driver utilisé pour les tests
+     */
     protected final WebDriver driver;
 
+    /**
+     * Le constructeur de BasePage, initialise le driver et les éléments
+     * @param webDriverManager Le WebDriverManager, récupéré via PicoContainer
+     */
     public BasePage(WebDriverManager webDriverManager) {
         this.driver = webDriverManager.getWebDriver();
         PageFactory.initElements(driver, this);
